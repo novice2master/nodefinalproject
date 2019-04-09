25f9193c0972271dceddb0e58566e704dbf27119
Joakins Onyango <joakinsonyango@joakinss-air.wifi.bcit.ca>
2019-04-09 12:50:43 -0700

    changed files

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const register = require('./register.js');
 const bodyparser = require('body-parser');
-// const mongodb = require('mongodb');
 const mongoose = require('mongoose')
-// var MongoClient = mongodb.MongoClient;
 
 
 var app = express();
@@ -14,30 +12,14 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static(__dirname, + '/public/'));
 hbs.registerPartials(__dirname + '/views/partials/');
 
-// var uri = "mongodb+srv://email:password@helpbook-rlpsi.gcp.mongodb.net/test?retryWrites=true";
-
-
-// var dbConn = MongoClient.connect(uri, { useNewUrlParser: true });
-// dbConn.then(function(client) {
-//     app.post('/signup', function (req, res) {
-//         delete req.body._id; // for safety reasons
-//         client.db("Users").collections("partners").insertOne(req.body);
-//         console.log('test');
-//     });
-// })
-// .catch(function(err){
-//     console.log(err)
-// });
-
-// var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/userdbase");
 
 var nameSchema = new mongoose.Schema({
-  FirstName: String,
-  LastName: String,
-  Email: String,
-  Password: String
+  fname: String,
+  lname: String,
+  email: {type: String, unique:true},
+  psw: String
 });
 
 var User = mongoose.model('User', nameSchema);
@@ -52,17 +34,19 @@ app.get('/login.hbs', (request, response) => {
     response.render('login.hbs');
 })
 
-app.post('./signup_form', (request, response) => {
-	var userInfo = new User(req.body);
+app.post('/signup_form', (request, response) => {
+	var userInfo = new User(request.body);
   	userInfo.save()
     .then(item => {
-      res.send("item saved to database");
+      response.render("confirm.hbs");
     })
     .catch(err => {
-      res.status(400).send("unable to save to database");
+      response.status(400).send("unable to save to database");
     });
 
 });
+
+
 
 app.get('/signup.hbs', (request, response) => {
     response.render('signup.hbs');
