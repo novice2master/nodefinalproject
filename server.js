8c5e73857f20b73de5a84becd65a319361fc5dc1
Joakins Onyango <joakinsonyango@joakinss-air.wifi.bcit.ca>
2019-04-09 12:56:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
 const register = require('./register.js');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose')
 
 
 var app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static(__dirname, + '/public/'));
 hbs.registerPartials(__dirname + '/views/partials/');
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/userdbase");
+
+var nameSchema = new mongoose.Schema({
+  fname: String,
+  lname: String,
+  email: {type: String, unique:true},
+  psw: String
+});
+
+var User = mongoose.model('User', nameSchema);
 
 app.set('view engine', 'hbs');
 
@@ -18,7 +34,19 @@ app.get('/login.hbs', (request, response) => {
     response.render('login.hbs');
 })
 
-app.post('/signup_form')
+app.post('/signup_form', (request, response) => {
+	var userInfo = new User(request.body);
+  	userInfo.save()
+    .then(item => {
+      response.render("confirm.hbs");
+    })
+    .catch(err => {
+      response.status(400).send("unable to save to database");
+    });
+
+});
+
+
 
 app.get('/signup.hbs', (request, response) => {
     response.render('signup.hbs');
