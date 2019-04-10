6509885f1f3d2c14dbba96e62f89b4b000a364ef
jdtm12 <48773359+jdtm12@users.noreply.github.com>
2019-04-09 18:10:44 -0700

    Login fixed.

--- server.js
+++ server.js
@@ -2,28 +2,18 @@ const express = require('express');
 const hbs = require('hbs');
 const register = require('./register.js');
 const bodyparser = require('body-parser');
-const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const MongoClient = require('mongodb').MongoClient;
+const utils = require('./utils');
 const md5 = require('md5')
 
 
 var app = express();
-
+app.use(cookieParser())
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static(__dirname, + '/public/'));
 hbs.registerPartials(__dirname + '/views/partials/');
-
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/userdbase");
-
-var nameSchema = new mongoose.Schema({
-  fname: String,
-  lname: String,
-  email: {type: String, unique:true},
-  psw: String
-});
-
-var User = mongoose.model('User', nameSchema);
 
 app.set('view engine', 'hbs');
 
@@ -36,18 +26,41 @@ app.get('/login.hbs', (request, response) => {
 })
 
 app.post('/signup_form', (request, response) => {
-	var userInfo = new User(request.body);
-  	userInfo.save()
-    .then(item => {
-      response.render("confirm.hbs");
-    })
-    .catch(err => {
-      response.status(400).send("unable to save to database");
-    });
+  var fname = request.body.fname;
+  var lname = request.body.lname;
+  var email = request.body.email;
+  var psw = request.body.psw;
+
+  var db = utils.getDb();
+  db.collection('users').insertOne({
+    First_Name: fname,
+    Last_Name: lname,
+    Email: email,
+    Password: psw
+  }, (err, result) => {
+    if(err) {
+      response.send('Unable to add user.');
+    }
+    response.render('confirm.hbs');
+  })
 });
 
-
-
+app.post('/login_form', (request, response) => {
+  var email = request.body.email;
+  var psw = request.body.psw;
+  var db = utils.getDb();
+  db.collection('users').findOne({Email: email, Password: psw}).then((doc)=>{
+    if(doc == null){
+      console.log('Login Failed')
+      response.render('login.hbs',{
+        login_error:'Incorrect login info'
+      })
+    }else{
+      response.cookie('username', doc.First_Name)
+      response.redirect('/');
+    }
+  })
+})
 
 app.get('/signup.hbs', (request, response) => {
     response.render('signup.hbs');
@@ -60,4 +73,5 @@ app.get('/confirm.hbs', (request, response) => {
 
 app.listen(8080, () => {
     console.log('Server is up on the port 8080');
+    utils.init();
 });
