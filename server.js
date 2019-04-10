2a7ec93cf5a716213ff820fc1b23841a594ded98
jdtm12 <48773359+jdtm12@users.noreply.github.com>
2019-04-09 18:44:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const MongoClient = require('mongodb').MongoClient;
 const utils = require('./utils');
 const md5 = require('md5')
 
-
 var app = express();
 app.use(cookieParser())
 app.use(bodyparser.json());
@@ -17,59 +16,69 @@ hbs.registerPartials(__dirname + '/views/partials/');
 
 app.set('view engine', 'hbs');
 
+//Homepage
 app.get('/', (request, response) => {
     response.render('index.hbs');
 })
 
+//Signup Page
+app.get('/signup.hbs', (request, response) => {
+  response.render('signup.hbs');
+  register.getElements;
+})
+
+//Signup Confirmation Page
+app.get('/confirm.hbs', (request, response) => {
+  response.render('confirm.hbs');
+})
+
+//Login Page
 app.get('/login.hbs', (request, response) => {
     response.render('login.hbs');
 })
 
+//Add user information to database
 app.post('/signup_form', (request, response) => {
-  var fname = request.body.fname;
-  var lname = request.body.lname;
-  var email = request.body.email;
-  var psw = request.body.psw;
+    var fname = request.body.fname;
+    var lname = request.body.lname;
+    var email = request.body.email;
+    var psw = request.body.psw;
 
-  var db = utils.getDb();
-  db.collection('users').insertOne({
-    First_Name: fname,
-    Last_Name: lname,
-    Email: email,
-    Password: psw
-  }, (err, result) => {
-    if(err) {
-      response.send('Unable to add user.');
-    }
-    response.render('confirm.hbs');
-  })
+    var db = utils.getDb();
+    db.collection('users').insertOne({
+      First_Name: fname,
+      Last_Name: lname,
+      Email: email,
+      Password: psw
+    }, (err) => {
+      if(err) {
+        response.send('Unable to add user.');
+      }
+      
+      response.render('confirm.hbs');
+    })
 });
 
+//Logs in user if they match information in database
 app.post('/login_form', (request, response) => {
-  var email = request.body.email;
-  var psw = request.body.psw;
-  var db = utils.getDb();
-  db.collection('users').findOne({Email: email, Password: psw}).then((doc)=>{
-    if(doc == null){
-      console.log('Login Failed')
-      response.render('login.hbs',{
-        login_error:'Incorrect login info'
-      })
-    }else{
-      response.cookie('username', doc.First_Name)
-      response.redirect('/');
-    }
-  })
+    var email = request.body.email;
+    var psw = request.body.psw;
+    var db = utils.getDb();
+    db.collection('users').findOne({Email: email, Password: psw}).then((doc)=>{
+      if(doc == null){
+        console.log('Login Failed')
+        response.render('login.hbs',{
+          login_error:'Incorrect login info'
+        })
+      } 
+    
+      else{
+        response.cookie('username', doc.First_Name)
+        response.redirect('/');
+      }
+    })
 })
 
-app.get('/signup.hbs', (request, response) => {
-    response.render('signup.hbs');
-    register.getElements;
-})
-
-app.get('/confirm.hbs', (request, response) => {
-    response.render('confirm.hbs');
-})
 
 app.listen(8080, () => {
     console.log('Server is up on the port 8080');
