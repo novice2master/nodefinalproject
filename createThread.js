8b4d0b62780de84412f7fa88b99c303e8572f9c5
muhash <tamikzamaev@gmail.com>
2019-05-02 18:10:22 -0700

    populated threads from json, deleted threads.hbs

--- createThread.js
+++ createThread.js
@@ -1,3 +1,24 @@
+var request = new XMLHttpRequest();
+request.open("GET", "threads.json", false);
+request.send(null);
+var obj = JSON.parse(request.responseText);
+
+
+function fillThreades(category) {
+    for (i in obj) {
+        if (obj[i].Category === category) {
+            addThread(obj[i].Category + String(i), "collapse" + category + String(i), "comments go here", obj[i]);
+        }
+    }
+}
+
+function fillThreadesAll() {
+    for (i in obj) {
+        addThread(obj[i].Category + String(i), "collapse" + obj[i].Category + String(i), "comments go here", obj[i]);
+    }
+}
+
+
 function addThread(heading, collapse, comments, object) {
     var threadlist = document.getElementById('generalThreads');
     threadlist.innerHTML += `
@@ -6,16 +27,13 @@ function addThread(heading, collapse, comments, object) {
                 <h5 class="mb-0">
                     <button class="btn btn-link" type="button"  aria-expanded="true" aria-controls=${collapse}>
                         ${object.Title}
+                        <small>&lt${object.Email}&gt</small>
                     </button>
                 </h5>
             </div>
 
             <div id=${collapse} class="collapse" aria-labelledby=${heading} data-parent="#generalThreads">
-
-
                 <div class="card-body">
-                    ${object.Email}
-                    <br>
                     ${object.Message}
                     <div class="row commentTopMargin">
                         <div class="col-md-4">&nbsp;</div>
