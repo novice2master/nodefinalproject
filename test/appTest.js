f53e76328b0cff5580b0f8d304f056f7ee78cc9b
Spyderkilla <Shravan1020@hotmail.com>
2019-05-02 18:07:27 -0700

    Update appTest.js

--- test/appTest.js
+++ test/appTest.js
@@ -1,98 +1,128 @@
+const request = require('supertest');
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-const app =require('../server');
-// Import the dependencies for testing
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-// const app = require('../server');
-// Configure chai
+const expect = require('chai').expect;
+// const db =require.requireActual('../utils');
+
+var chai = require('chai'), chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
-chai.should();
 
-describe("Threads", () => {
-    describe("GET /", () => {
-        // me test
-        // it("should get all threads", (done) => {
-        //     chai.request(app)
-        //         .get('/general_music.hbs')
-        //         .end((err, res) => {
-        //             res.should.have.status(200);
-        //             res.body.should.be.a('object');
-        //             done();
-        //         });
-        // });
-        // // Test to get all students record
-        it("Home page should load", (done) => {
-             chai.request(app);
-                 console.log(app)
-                 .get('/')
-                 .end((err, res) => {
-                     console.log(res);
-                     res.should.have.status(200);
-                     // res.body.should.be.a('object');
-                     done();
-                  });
-         });
-        // // Test to get single student record
-        // it("should get a single student record", (done) => {
-        //      const id = 1;
-        //      chai.request(app)
-        //          .get(`/${id}`)
-        //          .end((err, res) => {
-        //              res.should.have.status(200);
-        //              res.body.should.be.a('object');
-        //              done();
-        //           });
-        //  });
-        //
-        // // Test to get single student record
-        // it("should not get a single student record", (done) => {
-        //      const id = 5;
-        //      chai.request(app)
-        //          .get(`/${id}`)
-        //          .end((err, res) => {
-        //              res.should.have.status(404);
-        //              done();
-        //           });
-        //  });
-    });
-});
-//
-// sayHelloResult = app.sayHello();
-// addNumbersResult = app.addNumbers(5, 5);
+const app = require('../server');
 
-describe('App', function () {
-    // describe('sayHello()', function () {
-    //
-    //     it('sayHello should return hello', function () {
-    //         // let result = app.sayHello();
-    //         assert.equal(sayHelloResult, 'hello');
-    //     });
-    //
-    //     it('sayHello should return type string', function () {
-    //         // let result = app.sayHello();
-    //         assert.typeOf(sayHelloResult, 'string');
-    //     });
-    // });
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
+    }
+}
 
-
-    describe('addNumbers()', function () {
-
-        it('addNumbers should be above 5', function () {
-            // let result = app.addNumbers(5,5);
-            let addNumbersResult = app.addNumbers(5,5);
-            assert.isAbove(addNumbersResult, 5);
-        });
-
-        it('addNumbers should return type number', function () {
-            // let result = app.addNumbers(5,5);
-            let addNumbersResult = app.addNumbers(5,5);
-            assert.typeOf(addNumbersResult, 'number');
-        });
+describe('GET /', function () {
+    this.timeout(5000);
+    it("Main page test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
     });
 });
 
+describe('GET /general_music', function () {
+    this.timeout(5000);
+    it("General Music test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/general_music')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
+    });
+});
 
+describe('GET /latest_music', function () {
+    this.timeout(5000);
+    it("Latest Music page test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/latest_music')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
+    });
+});
 
+describe('GET /create_post', function () {
+    this.timeout(5000);
+    it("Crete Post test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/create_post')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
+    });
+});
 
+describe('GET /signup', function () {
+    this.timeout(5000);
+    it("Sign up test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/signup')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
+    });
+});
+
+describe('GET /confirm', function () {
+    this.timeout(5000);
+    it("confirm page test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/confirm')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
+    });
+});
+
+describe('GET /login', function () {
+    this.timeout(5000);
+    it("Main page test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/login')
+            .end(function(err, response) {
+                console.log(response.text);
+                expect(response).to.have.status(200);
+                done()
+            })
+    });
+});
+
+// describe('POST /signup_forum', function (){
+//     this.timeout(5000);
+//     it("Creating user sign up", function (done) {
+//         request(app).post('/signup_forum')
+//             .send({'fname' : 'Homer',
+//                     'lname': 'Simpson',
+//                     'email' : 'homer.simpson@gmail.com',
+//                     'psw' : 'homersimpson'})
+//             .then()
+//     })
+// })
