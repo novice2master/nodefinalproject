9cdd4caa732760af5fd6cb2681552dc255c40ed7
joakinsonyango <joakinsonyango05@gmail.com>
2019-05-07 21:30:43 -0700

    Update appTest.js

--- test/appTest.js
+++ test/appTest.js
@@ -19,36 +19,32 @@ function wait(ms){
     }
 }
 
-// describe('POST /signup', function () {
-//     // this.timeout(5000);
-//     let data = {
-//         "First_Name": "John",
-//         "Last_Name": "Mengi",
-//         "Email": "jmengi@lol.ke",
-//         "Password": "RegMengi123"
-//     }
-//     it("Account created", function (done) {
-//         wait(1000);
-//         request.agent(app)
-//             .post('/signup')
-//             .send(data)
-//             .expect(404)
-//             .end((err, response) => {
-//                 if (err) return done(err);
-//                 done()
-//             })
-//     });
-// });
+
+describe('POST /signup', function () {
+    let data = {
+        "First_Name": "John",
+        "Last_Name": "Mengi",
+        "Email": "jmengi@lol.ke",
+        "Password": "RegMengi123"
+    }
+    it("Account created", function (done) {
+        wait(1000);
+        request.agent(app).post('/signup')
+            .send(data)
+            .end((err, response) => {
+              expect('Location', '/confirmsignup');
+              if (err) return done(err);
+              done()
+            })
+    });
+});
 
 
 const data = {
         "Email": "jmengi@lol.ke",
-        "Password": "lol"
+        "Password": "RegMengi123"
     }
-
-
 before(function(done){
-    // this.timeout(5000);
       request.agent(app)
         .post('/login')
         .send(data)
@@ -58,35 +54,35 @@ before(function(done){
           done();
         });
 });
-
 describe('POST /login', function (done) {
     it("Account exists", function (done) {
         wait(1000);
-        request.agent(app).get('/login')
-        .expect(200, done);
+        request.agent(app).post('/login')
+        done();
   });
 });
-// describe('POST /thread_form', function () {
-//     this.timeout(1000);
-//     let data = {
-//         "Email": "jmengi@lol.ke",
-//         "Title": "Music",
-//         "Message": "Music today is lit",
-//         "Category": "General Music"
-//     };
-//     it("Account created", function (done) {
-//         wait(1000);
-//         chai.request(app)
-//             .post('/thread_form')
-//             .send(data)
-//             .end((err, response) => {
-//                 if (err) return done(err);
-//                 expect(response).to.have.status(200);
-//                 done()
-//             })
-//     });
+
+
+describe('POST /thread', function () {
+    this.timeout(1000);
+    let data = {
+        "Email": "jmengi@lol.ke",
+        "Title": "Music",
+        "Message": "Music today is lit",
+        "Category": "General Music"
+    };
+    it("Account created", function (done) {
+        wait(1000);
+        chai.request(app)
+            .post('/thread')
+            .send(data)
+            .end((err, response) => {
+                if (err) return done(err);
+                done()
+            })
+    });
     
-// });
+});
 
 // describe('POST /thread', function () {
 //     this.timeout(5000);
