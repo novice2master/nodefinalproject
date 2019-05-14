f3b91b2eaa292575d92befdfa8636f3c2ea6f4e2
ShravanHossan <Shravan1020@hotmail.com>
2019-05-14 15:17:13 -0700

    Revert "Merge branch 'back-end' of https://github.com/ShravanHossan/Agile-Development into back-end"
    This reverts commit b5a7517b24a1aeeafaef7fffeedda9810a261117, reversing
    changes made to 3f9873ef44f0780d74b0a75a35b8be2dc2d72d76.

--- server.js
+++ server.js
@@ -299,9 +299,16 @@ app.post('/addComment', async (request, response) => {
         comment: comment,
         email: email
     };
-
-    console.log(new_comment);
-    thread.Comments.unshift(new_comment);
+    if (typeof request.session.email !== "undefined") {
+        console.log('threadtest');
+        response.render('threads.hbs', {
+            disabled: null,
+            loggedin: "True",
+            email: request.session.email
+        })
+    } else {
+        thread.Comments.unshift(new_comment);
+    }
 
     db.collection('threads').findOneAndUpdate({
          _id: ObjectId(thread_id)
