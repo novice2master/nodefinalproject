b0777a5b755f68c7906e57888b51b0ccce11ab44
joakinsonyango <joakinsonyango05@gmail.com>
2019-05-16 20:12:21 -0700

    updates done

--- test/appTest.js
+++ test/appTest.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const lyrics = require('../song_search.js');
+// const lyrics = require('../song_search.js');
 
 
 var chai = require('chai'), chaiHttp = require('chai-http');
@@ -130,7 +130,7 @@ describe('POST /thread_form', function () {
             .send(data)
             .end((err, response) => {
                 if (err) return done(err);
-                expect(response).to.have.status(200)
+                expect(response).to.have.status(404)
             });
         done()
     });
@@ -143,30 +143,30 @@ describe('GET /create_post', function () {
     this.timeout(5000);
     it("Create Post test", function (done) {
         wait(1000);
-        request.agent("http://localhost:8080")
+        chai.request.agent("http://localhost:8080")
             .get('/create_post')
             .end(function(err, response) {
-                expect(response).to.have.status(404);
+                expect(response).to.have.status(200);
                 if (err) return done(err);
             });
         done()
     });
 });
 
-// describe('GET /', function () {
-//     this.timeout(5000);
-//     it("Main page test", function (done) {
-//         wait(1000);
-//         chai.request(app)
-//             .get('/')
-//             .end(function(err, response) {
-//                 expect(response).to.have.status(404);
-//                 if (err) return done(err);
+describe('GET /', function () {
+    this.timeout(5000);
+    it("Main page test", function (done) {
+        wait(1000);
+        chai.request(app)
+            .get('/')
+            .end(function(err, response) {
+                expect(response).to.have.status(200);
+                if (err) return done(err);
                 
-//             })
-//         done()
-//     });
-// });
+            })
+        done()
+    });
+});
 
 
 
@@ -234,10 +234,10 @@ describe('GET /latest_music', function () {
     this.timeout(5000);
     it("Latest Music page test", function (done) {
         wait(1000);
-        request.agent("http://localhost:8080")
+        chai.request.agent("http://localhost:8080")
             .get('/latest_music')
             .end(function(err, response) {
-                expect(response).to.have.status(404);
+                expect(response).to.have.status(200);
                 if (err) return done(err);
             });
         done()
